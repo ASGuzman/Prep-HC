8fe7c5aac5520a6f29f97284363d747bd9998ce6
Aylen SolGuzman <aylusolguzmanhoyos@gmail.com>
2023-08-08 20:01:43 -0300

    checkpoint commit

--- checkpoint.py
+++ checkpoint.py
@@ -12,7 +12,15 @@ def Factorial(numero):
         Factorial(-2) debe retornar nulo
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+    if (type(numero) != int):
+        return None
+    if numero < 1 :
+        return None
+    resultado_factorial = 1
+    while numero > 1 :
+        resultado_factorial = resultado_factorial * (numero)
+        numero = numero - 1
+    return resultado_factorial
 
 def EsPrimo(valor):
     '''
@@ -26,7 +34,15 @@ def EsPrimo(valor):
         EsPrimo(8) debe retornar False
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+    if type(valor) != int:
+        return None
+    es_primo = True
+    for i in range(2,valor):
+        if valor % i == 0:
+            es_primo = False
+            break
+    return es_primo
+    
     
 def ClaseAnimal(especie, color):
     '''
@@ -47,4 +63,13 @@ def ClaseAnimal(especie, color):
         a.CumpliAnios() -> debe devolver 3
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+    class Animal:
+        def __init__(self, especie, color):
+            self.edad = 0
+            self.especie = especie
+            self.color = color
+
+        def CumplirAnios (self):
+            self.edad = self.edad + 1
+            return self.edad
+    return Animal(especie, color)
